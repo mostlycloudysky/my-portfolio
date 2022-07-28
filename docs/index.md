a2dc4a76d1dca088a9bb513cfe694f57c36c0949
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2022-07-27 20:00:36 -0400

    initial site layout

---
+++ docs/index.md
@@ -0,0 +1,10 @@
+---
+title: 'Sandeep Yaramchitti'
+sidebar_label: Introduction
+description: 'I am a Software Engineer and Cloud Engineer passionate about solving Meaningful Problems.'
+slug: /
+---
+
+I am a Software Engineer and Cloud Engineer passionate about solving Meaningful Problems. 
+
+
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,133 +1,142 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-/** @type {import('@docusaurus/types').Config} */
-const config = {
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
   title: 'S.K.Y',
-  tagline: 'Life is what you make of it...',
+  tagline:
+    'I am a Software Engineer and Cloud Engineer passionate about solving Meaningful Problems in development and automation.',
   url: 'https://sandeepyaramchitti.com',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/braces.png',
-  themes: ['@docusaurus/theme-live-codeblock'],
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'SandeepKumarYaramchitti', // Usually your GitHub org/user name.
-  projectName: 'my-portfolio', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+  organizationName: 'SandeepKumarYaramchitti',
+  projectName: 'SandeepKumarYaramchitti',
+  themeConfig: {
+    navbar: {
+      title: 'S.K.Y',
+      logo: {
+        alt: 'sandeep-logo',
+        src: 'img/braces.png',
+      },
+      items: [
+        {
+          label: 'Docs',
+          position: 'left',
+          items: [
+            {
+              label: 'Development',
+              to: '/docs/development/',
+            },
+            {
+              label: 'AWS',
+              to: '/docs/aws/',
+            },
+            {
+              label: 'Web3.0',
+              to: '/docs/web3/',
+            },
+            {
+              label: 'Test Automation',
+              to: '/docs/test-automation/',
+            },
+          ],
+        },
+        {
+          label: 'Learn',
+          position: 'left',
+          items: [
+            {
+              label: 'Learn to Code',
+              to: '/docs/learn-to-code/',
+            },
+            {
+              label: 'Technology',
+              to: '/docs/technology/',
+            },
+            {
+              label: 'Reading List',
+              to: '/docs/reading-list/',
+            },
+          ],
+        },
+        {
+          label: 'Explore',
+          position: 'left',
+          items: [
+            {
+              label: 'Travel',
+              to: '/docs/travel/',
+            },
+            {
+              label: 'Music',
+              to: '/docs/music/',
+            },
+            {
+              label: 'Reading List',
+              to: '/docs/reading-list/',
+            },
+          ],
+        },
+        {
+          href: 'https://github.com/SandeepKumarYaramchitti/my-portfolio',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/SandeepKumarYaramchitti/my-portfolio',
+          label: 'Twitter',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Getting Started',
+              to: '/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.com/channels/1002001395618168893/1002001395618168896',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/sandeep01957280',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/SandeepKumarYaramchitti/my-portfolio',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Sandeep Yaramchitti`,
+    },
   },
-
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      '@docusaurus/preset-classic',
+      {
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/SandeepKumarYaramchitti/my-portfolio',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/SandeepKumarYaramchitti/my-portfolio',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'S.K.Y',
-        hideOnScroll: true,
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/braces.png',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          { to: '/blog', label: 'Blog', position: 'left' },
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © Sandeep Yaramchitti ${new Date().getFullYear()}`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
 };
-
-module.exports = config;
